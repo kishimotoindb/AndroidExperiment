fb8902e41b044d5761d9045ef5062669ef0839dc
cuihaichen <cuihaichen@xiaomi.com>
2020-07-22 18:23:12 +0800

    [PagingLibrary] Paging Library exercise

---
+++ app/src/main/java/com/fearlessbear/androidexperiment/components/paging/PagingListActivity.java
@@ -0,0 +1,15 @@
+package com.fearlessbear.androidexperiment.components.paging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fearlessbear.androidexperiment.R;
+
+public class PagingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paging_list);
+    }
+}
